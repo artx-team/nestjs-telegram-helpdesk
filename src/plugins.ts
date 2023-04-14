73d520572d0439c7980d5d9b783db1d20b3d7320
kovalenko <kovalenko@gmail.com>
2023-04-14 22:14:36 +0300

    fix: import plugins relative to process dir

--- src/plugins.ts
+++ src/plugins.ts
@@ -1,10 +1,12 @@
+import {normalize, sep} from 'path';
+
 import settings from '@/settings';
 
 export const plugins = [];
 
 export async function loadPlugins(): Promise<void> {
   if (settings.plugins.length > 0) {
-    const loaded = await Promise.all(settings.plugins.map(m => import(m)));
+    const loaded = await Promise.all(settings.plugins.map(m => import(normalize(process.cwd() + sep + m))));
 
     loaded.forEach(m => plugins.push(m.default));
   }
